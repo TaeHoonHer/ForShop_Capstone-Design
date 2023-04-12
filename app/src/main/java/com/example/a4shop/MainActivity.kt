bbb0330a87befe752673b08857d4974ad7e174ff
j2an777 <ha991104@gmail.com>
2023-04-12 21:44:22 +0900

    카메라 기본 기능 구현1

--- app/src/main/java/com/example/a4shop/MainActivity.kt
+++ app/src/main/java/com/example/a4shop/MainActivity.kt
@@ -1,45 +1,39 @@
 package com.example.a4shop
 
 import android.Manifest
-import android.content.ContentUris
-import android.content.ContentValues.TAG
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
-import android.content.Intent.ACTION_PICK
 import android.content.pm.PackageManager
 import android.graphics.*
 import android.hardware.Camera
-import android.net.Uri
+import android.media.ExifInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.*
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Switch
 import android.widget.Toast
-import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
-import androidx.core.graphics.drawable.toBitmap
-import com.bumptech.glide.Glide
-import com.example.a4shop.databinding.ActivityGalleryBinding
+import androidx.core.net.toUri
+import com.bumptech.glide.Glide.init
 import com.example.a4shop.databinding.ActivityMainBinding
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
-private const val REQUEST_CODE_FOR_IMAGE_CAPTURE = 100
-private const val TAG = "MainActivity"
-
 class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.PictureCallback {
     lateinit var binding: ActivityMainBinding
-    private lateinit var photoFile: File
 
     private var camera: Camera? = null
+    private var cameraId = Camera.CameraInfo.CAMERA_FACING_FRONT
     private var surfaceHolder: SurfaceHolder? = null
     private var showGrid = false
     val GET_GALLERY_IMAGE = 200
@@ -76,7 +70,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.Picture
 
         // 촬영 버튼 click listener 추가
         binding.buttonTakePicture.setOnClickListener {
-            camera?.takePicture(null, null, this)
+            takePhotoAndSave(this)
         }
 
         // 토글버튼 구현
@@ -84,6 +78,11 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.Picture
             if(isChecked) {
                 binding.guidelineBtn.isChecked = true
             }
+        }
+
+        // 카메라 전환 버튼
+        binding.reverseBtn.setOnClickListener {
+            switchCamera()
         }
 
     }
@@ -146,6 +145,117 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.Picture
             }
         } finally {
             surfaceHolder?.unlockCanvasAndPost(canvas)
+        }
+    }
+
+    // 카메라 후면 전환 메소드
+    private fun switchCamera() {
+        try {
+            // 현재 카메라 정보 가져오기
+            val cameraInfo = Camera.CameraInfo()
+            Camera.getCameraInfo(cameraId, cameraInfo)
+
+            // 현재 카메라의 전/후면 여부 확인
+            val isBackCamera = cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK
+
+            // 카메라 사용 중지
+            camera?.stopPreview()
+            camera?.release()
+            camera = null
+
+            // 전/후면 카메라 전환
+            cameraId = if (isBackCamera) Camera.CameraInfo.CAMERA_FACING_FRONT else Camera.CameraInfo.CAMERA_FACING_BACK
+
+            // 전환된 카메라로 다시 카메라 사용 시작
+            camera = Camera.open(cameraId)
+            camera?.setDisplayOrientation(90) // 카메라 미리보기 화면 회전 설정
+            camera?.setPreviewDisplay(surfaceHolder) // 카메라 미리보기 화면 설정
+            camera?.startPreview()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    // 버튼 클릭 시 사진 촬영 및 MediaStore에 저장
+    private fun takePhotoAndSave(context: Context) {
+
+        camera?.takePicture(null, null, { data, camera ->
+            // 사진 촬영 후 처리할 동작
+            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+            val fileName = "IMG_$timeStamp.jpg"
+            val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val imageFile = File(storageDir, fileName)
+
+            // 이미지 파일 저장
+            try {
+                val fos = FileOutputStream(imageFile)
+                fos.write(data)
+                fos.close()
+
+                // Exif 정보 확인하여 이미지 회전 처리
+                val exif = ExifInterface(imageFile.absolutePath)
+                val rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+                val rotationInDegrees = exifToDegrees(rotation)
+                val bitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
+                val rotatedBitmap = rotateBitmap(bitmap, rotationInDegrees)
+                val outputStream = FileOutputStream(imageFile)
+                rotatedBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.flush()
+                outputStream.close()
+
+                // MediaStore에 이미지 파일 등록
+                val contentResolver: ContentResolver = context.contentResolver
+                val values = ContentValues().apply {
+                    put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+                    put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                    put(MediaStore.Images.Media.DATA, imageFile.absolutePath)
+                }
+                contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+                // 사진 찍힌 이미지를 갤러리에 나타내기 위해 스캔
+                val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+                mediaScanIntent.data = imageFile.toUri()
+                context.sendBroadcast(mediaScanIntent)
+
+                Toast.makeText(context, "사진이 저장되었습니다.", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Log.e("TAG", "사진 저장 실패: ${e.message}")
+                Toast.makeText(context, "사진 저장 실패", Toast.LENGTH_SHORT).show()
+            }
+
+            // 카메라 프리뷰 재개
+            camera.startPreview()
+        })
+    }
+
+    // Exif 회전 정보를 각도로 변환
+    private fun exifToDegrees(rotation: Int): Int {
+        return when (rotation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> 90
+            ExifInterface.ORIENTATION_ROTATE_180 -> 180
+            ExifInterface.ORIENTATION_ROTATE_270 -> 270
+            else -> 0
+        }
+    }
+
+    // 이미지를 회전하는 함수
+    private fun rotateBitmap(bitmap: Bitmap, degrees: Int): Bitmap? {
+        if (degrees == 0) {
+            return bitmap
+        }
+
+        val matrix = Matrix()
+        matrix.setRotate(degrees.toFloat(), bitmap.width.toFloat() / 3, bitmap.height.toFloat() / 3)
+
+        return try {
+            val rotatedBitmap =
+                Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+            bitmap.recycle()
+            rotatedBitmap
+        } catch (e: OutOfMemoryError) {
+            e.printStackTrace()
+            null
         }
     }
 
