3ecc959aa420303f9294c8d10af8ec3afd854624
황성태 <vivian991031@naver.com>
2023-04-18 15:14:16 +0900

    enum class 작성
    추후에 필요할 것 같아 생성해뒀습니다.

---
+++ Server/project/src/main/java/com/forshop/project/domain/type/SearchType.java
@@ -0,0 +1,18 @@
+package com.forshop.project.domain.type;
+
+import lombok.Getter;
+
+public enum SearchType {
+    TITLE("제목"),
+    CONTENT("본문"),
+    ID("유저 ID"),
+    NICKNAME("닉네임"),
+    HASHTAG("해시태그");
+
+    @Getter
+    private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
+}
