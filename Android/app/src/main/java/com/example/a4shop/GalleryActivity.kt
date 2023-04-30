e9dde4f139859dfa9777a8bc1b246f323a0551a7
j2an777 <ha991104@gmail.com>
2023-04-30 18:17:04 +0900

    동적버튼 캐시메모리로 저장

--- Android/app/src/main/java/com/example/a4shop/GalleryActivity.kt
+++ Android/app/src/main/java/com/example/a4shop/GalleryActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a4shop
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
@@ -10,15 +11,20 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.a4shop.databinding.ActivityGalleryBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.json.JSONArray
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.FileReader
 import java.io.FileWriter
 
+private const val FARRAY_PREFS_NAME = "FArrayPrefs"
+private const val FARRAY_KEY = "FArrayKey"
 class GalleryActivity : AppCompatActivity() {
     lateinit var binding: ActivityGalleryBinding
     private lateinit var filterAdapter : FilterAdapter
+    var FArray = mutableListOf<Fcontents>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,8 +58,7 @@ class GalleryActivity : AppCompatActivity() {
             }
         }
 
-        var FArray = mutableListOf<Fcontents>()
-
+        loadFArray()
         filterAdapter = FilterAdapter(FArray) { content ->
             // 눌렀을때 키워드 관련 이미지들 보이게 하기
         }
@@ -69,6 +74,7 @@ class GalleryActivity : AppCompatActivity() {
             val dlg = MyDialog(this) { value ->
                 FArray.add(Fcontents("#"+value))
                 filterAdapter.notifyDataSetChanged()
+                saveFArray()
             }
 
             dlg.show()
@@ -79,7 +85,6 @@ class GalleryActivity : AppCompatActivity() {
         when (item.getItemId()) {
             android.R.id.home -> startActivity(Intent(this, MainActivity::class.java))
             R.id.gallerySearch -> {
-                binding.galleryTitle.visibility = View.GONE;
                 // gallerySearch MenuItem이 선택되었을 때의 동작을 구현
                 val searchView = item.actionView as androidx.appcompat.widget.SearchView
                 searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
@@ -98,6 +103,7 @@ class GalleryActivity : AppCompatActivity() {
                         return false
                     }
                 })
+                binding.galleryTitle?.visibility = View.GONE;
                 return true
             }
         }
@@ -115,5 +121,24 @@ class GalleryActivity : AppCompatActivity() {
         // 검색어를 이용한 실제 검색 로직을 구현
         // 검색 결과를 처리하는 부분을 구현
         Toast.makeText(this, "검색어: $query", Toast.LENGTH_SHORT).show()
+    }
+
+    // FArray를 저장하는 함수
+    private fun saveFArray() {
+        val prefs = getSharedPreferences(FARRAY_PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        val json = Gson().toJson(FArray) // Gson을 이용하여 FArray를 JSON 형태로 변환
+        editor.putString(FARRAY_KEY, json)
+        editor.apply()
+    }
+
+    // FArray를 불러오는 함수
+    private fun loadFArray() {
+        val prefs = getSharedPreferences(FARRAY_PREFS_NAME, Context.MODE_PRIVATE)
+        val json = prefs.getString(FARRAY_KEY, null)
+        if (json != null) {
+            val type = object : TypeToken<ArrayList<Fcontents>>() {}.type
+            FArray = Gson().fromJson(json, type) // JSON을 FArray로 변환
+        }
     }
 }
